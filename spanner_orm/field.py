694fe1e882ea2f00a64d826aded2876defdc7bf7
jtoss <julio.toss@unico.io>
2021-08-06 14:22:12 -0300

    Feature/add bytes type (#2)
    * ADD: bytes field type
    
    * ADD: unit tests creating BYTES field.

--- spanner_orm/field.py
+++ spanner_orm/field.py
@@ -202,6 +202,23 @@ class Bytes(FieldType):
             raise error.ValidationError("{} is not of type bytes".format(value))
 
 
+class Bytes(FieldType):
+    """Represents a bytes type."""
+
+    @staticmethod
+    def ddl(size="MAX") -> str:
+        return "BYTES({size})".format(size=size)
+
+    @staticmethod
+    def grpc_type() -> type_pb2.Type:
+        return type_pb2.Type(code=type_pb2.BYTES)
+
+    @staticmethod
+    def validate_type(value) -> None:
+        if not isinstance(value, bytes):
+            raise error.ValidationError("{} is not of type bytes".format(value))
+
+
 class Date(FieldType):
     """Represents a date type."""
 
